1df364e6b89262111b51075ebd0c6a8ca24274eb
wagiminator <wagiminator@web.de>
2023-03-26 12:24:34 +0200

    Some tweaks

--- software/nrf2cdc/include/config.h
+++ software/nrf2cdc/include/config.h
@@ -20,8 +20,8 @@
 #define CMD_IDENT           '!'       // command string identifier
 
 // USB device descriptor
-#define USB_VENDOR_ID       0x6666    // VID: Prototype
-#define USB_PRODUCT_ID      0x6666    // PID: Prototype
+#define USB_VENDOR_ID       0x16C0    // VID (shared www.voti.nl)
+#define USB_PRODUCT_ID      0x27DD    // PID (shared CDC)
 #define USB_DEVICE_VERSION  0x0100    // v1.0 (BCD-format)
 
 // USB configuration descriptor
